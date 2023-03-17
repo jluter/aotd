25270d6c7f46dfe30395d6d10ced9cffdc278b95
Jakob Luter <jakobluter20@gmail.com>
2023-03-16 21:12:58 -0500

    Add simple form in TypeScript to submit a form and handle form submission.

--- src/Pages/AotdPage/AotdPage.tsx
+++ src/Pages/AotdPage/AotdPage.tsx
@@ -3,12 +3,22 @@ import './AotdPage.scss';
 import AotdHeader from '../../Components/AotdHeader/AotdHeader';
 
 class AotdPage extends Component {
+
+    handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(event);
+    }
+
     render() {
 
         return (
             <main className='aotd'>
-            <AotdHeader />
-        </main>
+                <AotdHeader />
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder='Artist name'></input>
+                    <button type='submit'>Submit</button>
+                </form>
+            </main>
     );
     }
 };
