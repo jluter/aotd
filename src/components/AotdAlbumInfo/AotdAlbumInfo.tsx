b30fb3896be31de91903f206826752c2680a1fa1
Jakob Luter <jakobluter20@gmail.com>
2023-07-03 18:27:10 -0500

    Add styling and display for albums on Artist click.

--- src/components/AotdAlbumInfo/AotdAlbumInfo.tsx
+++ src/components/AotdAlbumInfo/AotdAlbumInfo.tsx
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './AotdAlbumInfo.scss';
 import ArtistSearchResults from '../ArtistSearchResults/ArtistSearchResults';
-import axios from 'axios';
 
 interface Props {
     getArtistData: [];
@@ -11,11 +10,34 @@ interface Props {
 
 const AotdAlbumInfo: React.FC<Props> = ({ getArtistData, albumData, handleArtistClick }) => {
 
+    const returnedAlbums = albumData.map(album => {
+
+        //error handling to make sure all required fields are present
+        if (!album['name'] || !album['image'] || !album['id']) {
+            return null;
+        }
+
+        //requires bracket notation due to issues with microsoft's typescript compiler
+        let albumName = album['name'];
+        let albumImg = album['image'];
+        let albumId = album['id'];
+        
+        return (
+            <figure key={albumId} className='album-search-results' onClick={() => {console.log('clicked')}}>
+                <img className="album-search-results__img" src={albumImg} alt={`Album cover art for` + {albumName}}/>
+                <p className='album-search-results__name'>{albumName}</p>
+            </figure>
+        )
+    })
+
+
     return (
         <div>
             {/* Needs to properly pass state from AotdPage so that when a new artist is searched, it shows <ArtistSearchResults> again, instead of the album list */}
             { albumData.length > 0 
-            ? <p>{JSON.stringify(albumData)}</p>
+            ? <section className="returned-album-container">
+                {returnedAlbums}
+            </section>
             : <ArtistSearchResults handleArtistClick={handleArtistClick} getArtistData={getArtistData} albumData={albumData}/>
             } 
         </div>
