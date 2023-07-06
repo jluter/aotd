a344c6a7f5c869f969f3a3216cea67b799b4b0e7
Jakob Luter <jakobluter20@gmail.com>
2023-07-05 22:27:59 -0500

    add html structure and css styling to add album modal.

--- src/components/AddAlbumModal/AddAlbumModal.tsx
+++ src/components/AddAlbumModal/AddAlbumModal.tsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 interface Props {
     // addAlbumToAotd: (id: string) => void;
     showMe: string;
-    album: Object;
+    album: {artist: [], id: string, image: string, name: string};
     setShowMe: (showMe: string) => void
 }
 
@@ -18,22 +18,31 @@ const AddAlbumModal: React.FC<Props> = ({ showMe, album, setShowMe}) => {
         })
         .then((response) => {
             console.log(response);
-
         })
         .catch((error) => {
             console.log(error)
         })    
+
+        setShowMe('false');
     };
 
     return (
         <div id="add-album-modal" className={showMe}>
-            <h2>Are you sure you want to add this album? </h2>
-            <div>Insert album info here</div>
+            <section className="modal-album">
+
+            <h2 className="modal-album__question">Are you sure you want to add this album? </h2>
+            <div className="modal-album__info">
+                <h1 className='modal-album__title'>
+                    {album.name}
+                </h1>
+                <img width='200' src={album.image}/>
+            </div>
                 <div>
                     {/* Add function to add album to album of the day list on node.js server */}
-                    <button onClick={() => {addAlbumToAotd(album)}}>Yes</button>
-                    <button onClick={() => setShowMe('false')}>No</button>
+                    <button type="button" onClick={() => {addAlbumToAotd(album); console.log(album);}}>Yes</button>
+                    <button type="button" onClick={() => setShowMe('false')}>No</button>
                 </div>
+            </section>
         </div>
     );
 };
