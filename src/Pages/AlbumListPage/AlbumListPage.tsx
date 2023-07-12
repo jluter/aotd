1e8de2932f966e93a509944492484f6744d51002
Jakob Luter <jakobluter20@gmail.com>
2023-07-11 21:17:16 -0500

    Move albumList to AlbumList.tsx component. Add scss stylings to albumlist.

--- src/Pages/AlbumListPage/AlbumListPage.tsx
+++ src/Pages/AlbumListPage/AlbumListPage.tsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import './AlbumListPage.scss';
 import axios from 'axios';
+import AlbumList from '../../Components/AlbumList/AlbumList';
 
 
-interface Props {
-
-}
+interface Props {}
 
 interface State {
     aotdList: []
@@ -34,27 +33,13 @@ class AlbumListPage extends Component<Props, State> {
  
     render() {
 
-        //assigning the album list state to a mapped object
-        const albumsOfTheDay = this.state.aotdList.map((album: {artists: [], id: string, name: string, image: string, time: string, uuid: string}) => {
 
-            //Adjust what this variable will look like
-            return  <div className='album' key={album.uuid}>
-                        <div className='album__name'>{album.name}</div>
-                        <div className='album__image-container'>
-                            <img className='album__image' src={album.image} alt='Album cover'/>
-                        </div>
-                    </div>;
-        })
-
-
-        return (
-          <main className="aotd">
-            Album of the Day List
-            <section className='album-list-container'>
-                {albumsOfTheDay}
-            </section>
-          </main>
-        );
+      return (
+        <main className="aotd">
+          Album of the Day List
+        <AlbumList aotdList={this.state.aotdList}/>
+        </main>
+      );
     }
 };
 
--- src/components/AlbumList/AlbumList.tsx
+++ src/components/AlbumList/AlbumList.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 import './AlbumList.scss';
 
-const AlbumList: React.FC = () => {
+interface Props {
+    aotdList: [];
+}
+
+const AlbumList: React.FC<Props> = ({aotdList}) => {
+
+    
+    
     return (
-        <div>
-            
-        </div>
+      <section className="album-list-container">
+
+        {/* assigning the album list state to a mapped object */}
+        {aotdList.map(
+          (album: {
+            artists: [];
+            id: string;
+            name: string;
+            image: string;
+            time: string;
+            uuid: string;
+          }) => {
+            return (
+
+              <div className="album" key={album.uuid}>
+                <div className="album__image-container">
+                  <img
+                    className="album__image"
+                    src={album.image}
+                    alt="Album cover"
+                  />
+                </div>
+                <div className="album__name">{album.name}</div>
+                <div className='album__date'>{album.time}</div>
+              </div>
+            );
+          }
+        )}
+
+      </section>
     );
 };
 
